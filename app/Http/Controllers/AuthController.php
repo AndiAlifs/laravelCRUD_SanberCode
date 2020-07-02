39e9b5680a14ffe0de00eb35868da131ca18d9ff
Andi Alifsyach <andyalyfsyah4@gmail.com>
2020-07-03 00:31:51 +0800

    inital commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register()
+    {
+        return view("register");
+    }
+
+    public function welcome(Request $request)
+    {   
+        return view("welcam", ["first_name" => $request["first_name"],"last_name" => $request["last_name"]]);
+    }
+}
