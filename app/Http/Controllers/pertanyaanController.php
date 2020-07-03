b4321c2140e32a7ddd398cdfad5ba4d59bdc7dbf
Andi Alifsyach <andyalyfsyah4@gmail.com>
2020-07-03 17:53:51 +0800

    add controller

---
+++ app/Http/Controllers/pertanyaanController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\pertanyaanModel;
+
+class pertanyaanController extends Controller{
+
+    public function index()
+    {   
+        $all_item = pertanyaanModel::get_all();
+        return view('pertanyaan.index',compact('all_item'));
+    }
+
+    public function create()
+    {
+        return view('pertanyaan.create');
+    }
+
+    public function store(Request $request)
+    {   
+        $array_baru = array(
+            'judul' => $request['judul'], 
+            'isi' => $request['isi'], 
+        );
+        $new_item = pertanyaanModel::save($array_baru);
+        return redirect('/pertanyaan');
+    }
+}
