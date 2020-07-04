8b34ee55e49bd2ef3e37b6d3b1010e17bb9f89e2
Andi Alifsyach <andyalyfsyah4@gmail.com>
2020-07-04 09:20:49 +0800

    add jawaban controller

--- app/Http/Controllers/jawabanController.php
+++ app/Http/Controllers/jawabanController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\jawabanModel;
+
 
 class jawabanController extends Controller
 {
-    //
+    public function index($id)
+    {
+        $items = jawabanModel::indexById($id);
+        return view('jawaban.index',compact('items'));
+    }
+
+    public function store(Request $request,$id)
+    {   
+        $array_baru = array(
+            'pertanyaan_id' => $id, 
+            'isi' => $request['isi'],
+        );
+        $new_item = jawabanModel::save($array_baru);
+        return redirect('/jawaban/'.$id);
+    }
 }
